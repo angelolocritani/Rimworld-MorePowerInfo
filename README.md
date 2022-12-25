8ab61e008ec79083e7158a34d5b4b312bac62916
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2022-12-25 11:21:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Version](https://img.shields.io/badge/Rimworld-1.4-green.svg)](http://rimworldgame.com/)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/angelolocritani/Rimworld-MorePowerInfo)](https://github.com/angelolocritani/Rimworld-MorePowerInfo/releases/latest)
+
+# More power info
+Displays more power informations
+
+[![Steam download](https://img.shields.io/steam/downloads/2906529763?logo=steam)](https://steamcommunity.com/sharedfiles/filedetails/?id=2906529763)
---
+++ Source/MorePowerInfo/MorePowerInfo.cs
@@ -0,0 +1,104 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using System.Reflection;
+using HarmonyLib;
+using System;
+
+namespace ExtraPowerInfo
+{
+    [StaticConstructorOnStartup]
+    public static class ExtraPowerInfoMod
+    {
+        public static Harmony harmonyInstance;
+        static ExtraPowerInfoMod()
+        {
+            harmonyInstance = new Harmony("arl85.MorePowerInfo");
+            harmonyInstance.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(CompPower), "CompInspectStringExtra")]
+        public static class CompPower_CompInspectStringExtra
+        {
+            static void Postfix(ref string __result, CompPower __instance, float ___WattsToWattDaysPerTick)
+            {
+
+                PowerNet powerNet = __instance.PowerNet;
+                if (powerNet != null)
+                {
+                    float energyStored = powerNet.CurrentStoredEnergy();
+
+                    float energyUsageTick = 0f;
+                    float energyProductionTick = 0f;
+                    float energyBalanceTick;
+
+                    for (int i = 0; i < powerNet.powerComps.Count; i++)
+                    {
+                        if (powerNet.powerComps[i].PowerOn && powerNet.powerComps[i].EnergyOutputPerTick != 0f)
+                        {
+
+                            //Log.Message(powerNet.powerComps[i].parent.def.defName.ToString() + ": " + powerNet.powerComps[i].EnergyOutputPerTick);
+
+                            if (powerNet.powerComps[i].EnergyOutputPerTick < 0f)
+                            {
+                                energyUsageTick -= powerNet.powerComps[i].EnergyOutputPerTick;
+                            }
+                            else
+                            {
+                                energyProductionTick += powerNet.powerComps[i].EnergyOutputPerTick;
+                            }
+                        }
+                    }
+                    energyBalanceTick = energyProductionTick - energyUsageTick;
+
+
+                    if (Math.Abs(energyBalanceTick) < 0.00001) energyBalanceTick = 0; //dirty trick to avoid floating points rounding errors
+                    Log.Message("ept: " + energyProductionTick + " eut: " + energyUsageTick + " ebt: " + energyBalanceTick + "es: " + energyStored);
+
+                    int currentAutonomyTicks = 0;
+                    int storageOnlyAutonomyTicks = 0;
+
+                    string currentAutonomyStr = string.Empty;
+                    string storageOnlyAutonomyStr = string.Empty;
+
+                    if (energyStored > 0) /* no power stored --> 0 autonomy */
+                    {
+
+                        if (energyBalanceTick >= 0) /* positive energy balance -> infinite autonomy */
+                        {
+                            currentAutonomyStr = "MorePowerInfo.Forever".Translate();
+                        }
+                        else
+                        {
+                            currentAutonomyTicks = (int)(energyStored / -energyBalanceTick);
+                        }
+
+                        storageOnlyAutonomyTicks = (int)(energyStored / energyUsageTick);
+
+                    }
+
+                    /* string conversion */
+                    if (currentAutonomyStr == string.Empty) currentAutonomyStr = currentAutonomyTicks.ToStringTicksToPeriod();
+                    storageOnlyAutonomyStr = storageOnlyAutonomyTicks.ToStringTicksToPeriod();
+                    string energyStoredStr = energyStored.ToString("F0");
+                    string energyProductionStr = (energyProductionTick / ___WattsToWattDaysPerTick).ToString("F0");
+                    string energyUsageStr = (energyUsageTick / ___WattsToWattDaysPerTick).ToString("F0");
+                    string energyBalanceStr = (energyBalanceTick / ___WattsToWattDaysPerTick).ToString("F0");
+
+
+                    __result = "MorePowerInfo.Production".Translate(energyProductionStr, energyUsageStr, energyBalanceStr) + "\n" +
+                               //"MorePowerInfo.Storage".Translate(energyStoredStr, currentAutonomy, worstCaseAutonomy);
+                               "MorePowerInfo.Storage".Translate(energyStoredStr, currentAutonomyStr) + "\n" +
+                               "MorePowerInfo.BatteryOnly".Translate(storageOnlyAutonomyStr);
+
+
+                }
+
+            }
+        }
+
+    }
+
+}
+
