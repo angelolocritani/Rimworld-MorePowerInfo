515a228f4bee23a570221fadaaac702e4cfd5e46
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2022-12-27 22:46:14 +0100

    Now considering battery self-discharging effect
    Most of the times this effect is minimal but may addup if you have several batteries

--- Source/MorePowerInfo/MorePowerInfo.cs
+++ Source/MorePowerInfo/MorePowerInfo.cs
@@ -6,11 +6,16 @@ using System.Reflection;
 using HarmonyLib;
 using System;
 
+
+
 namespace ExtraPowerInfo
 {
+
     [StaticConstructorOnStartup]
     public static class ExtraPowerInfoMod
     {
+        public const float MinDifference = 0.00001f;
+
         public static Harmony harmonyInstance;
         static ExtraPowerInfoMod()
         {
@@ -21,7 +26,7 @@ namespace ExtraPowerInfo
         [HarmonyPatch(typeof(CompPower), "CompInspectStringExtra")]
         public static class CompPower_CompInspectStringExtra
         {
-            static void Postfix(ref string __result, CompPower __instance, float ___WattsToWattDaysPerTick)
+            static void Postfix(ref string __result, CompPower __instance)
             {
 
                 PowerNet powerNet = __instance.PowerNet;
@@ -30,6 +35,7 @@ namespace ExtraPowerInfo
                     float energyStored = powerNet.CurrentStoredEnergy();
 
                     float energyUsageTick = 0f;
+                    float batterySelfDischargeUsageTick = 0f;
                     float energyProductionTick = 0f;
                     float energyBalanceTick;
 
@@ -48,13 +54,14 @@ namespace ExtraPowerInfo
                             }
                         }
                     }
-                    energyBalanceTick = energyProductionTick - energyUsageTick;
+                    energyBalanceTick = Math.Abs(energyProductionTick - energyUsageTick) < MinDifference ? 0 : energyProductionTick - energyUsageTick; //dirty trick to avoid floating points rounding errors
 
 
-                    if (Math.Abs(energyBalanceTick) < 0.00001) energyBalanceTick = 0; //dirty trick to avoid floating points rounding errors
-#if DEBUG
-                    Log.Message("ept: " + energyProductionTick + " eut: " + energyUsageTick + " ebt: " + energyBalanceTick + "es: " + energyStored);
-#endif
+                    Utility.LogEveryNTicks("energyProductionTick: " + energyProductionTick);
+                    Utility.LogEveryNTicks("energyUsageTick: " + energyUsageTick);
+                    Utility.LogEveryNTicks("energyBalanceTick: " + energyBalanceTick);
+                    Utility.LogEveryNTicks("energyStored: " + energyStored);
+
                     int currentAutonomyTicks = 0;
                     int storageOnlyAutonomyTicks = 0;
 
@@ -63,41 +70,55 @@ namespace ExtraPowerInfo
 
                     if (energyStored > 0) /* no power stored --> 0 autonomy */
                     {
+                        for (int i = 0; i < powerNet.batteryComps.Count; i++) //self-discharging batteries
+                        {
+                            if (powerNet.batteryComps[i].StoredEnergy > 0)
+                            {
+                                batterySelfDischargeUsageTick += Mathf.Min(5f * CompPower.WattsToWattDaysPerTick, powerNet.batteryComps[i].StoredEnergy);
 
-                        if (energyBalanceTick >= 0) /* positive energy balance -> infinite autonomy */
+                            }
+                        }
+
+                        Utility.LogEveryNTicks("batterySelfDischargeUsageTick: " + batterySelfDischargeUsageTick);
+
+                        //battery flow => (half the EXTRA energy OR negative energy) - 5Wd per battery
+                        float batteryFlowTick = (energyBalanceTick >= 0
+                                            ? energyBalanceTick / 2  //maybe use CompProperties_Battery Props.efficiency (0.5) instead of hardcoding it here
+                                            : energyBalanceTick)
+                                            - batterySelfDischargeUsageTick;
+
+                        if (Math.Abs(batteryFlowTick) < MinDifference) batteryFlowTick = 0;
+
+                        Utility.LogEveryNTicks("batteryFlow: " + batteryFlowTick);
+
+                        if (batteryFlowTick >= 0) /* positive battery flow -> infinite autonomy */
                         {
                             currentAutonomyStr = "MorePowerInfo.Forever".Translate();
                         }
                         else
                         {
-                            currentAutonomyTicks = (int)(energyStored / -energyBalanceTick);
+                            currentAutonomyTicks = (int)(energyStored / -batteryFlowTick);
                         }
-
-                        if (energyUsageTick == 0) /* no energy usage (ie: disconnected battery) -> infinite autonomy */
-                        {
-                            storageOnlyAutonomyStr = "MorePowerInfo.Forever".Translate();
-                        }
-                        else
-                        {
-                            storageOnlyAutonomyTicks = (int)(energyStored / energyUsageTick);
-                        }
+                        // all energy usage + the self discharge energy of batteries
+                        storageOnlyAutonomyTicks = (int)(energyStored / (energyUsageTick + batterySelfDischargeUsageTick));
 
                     }
 
                     /* string conversion */
                     if (currentAutonomyStr == string.Empty) currentAutonomyStr = currentAutonomyTicks.ToStringTicksToPeriod();
-                    if (storageOnlyAutonomyStr == string.Empty)  storageOnlyAutonomyStr = storageOnlyAutonomyTicks.ToStringTicksToPeriod();
+                    if (storageOnlyAutonomyStr == string.Empty) storageOnlyAutonomyStr = storageOnlyAutonomyTicks.ToStringTicksToPeriod();
+
                     string energyStoredStr = energyStored.ToString("F0");
-                    string energyProductionStr = (energyProductionTick / ___WattsToWattDaysPerTick).ToString("F0");
-                    string energyUsageStr = (energyUsageTick / ___WattsToWattDaysPerTick).ToString("F0");
-                    string energyBalanceStr = (energyBalanceTick / ___WattsToWattDaysPerTick).ToString("F0");
+                    string energyProductionStr = (energyProductionTick / CompPower.WattsToWattDaysPerTick).ToString("F0");
+                    string energyUsageStr = (energyUsageTick / CompPower.WattsToWattDaysPerTick).ToString("F0");
+                    string energyBalanceStr = (energyBalanceTick / CompPower.WattsToWattDaysPerTick).ToString("F0");
 
 
                     __result = "MorePowerInfo.Production".Translate(energyProductionStr, energyUsageStr, energyBalanceStr) + "\n" +
-                               //"MorePowerInfo.Storage".Translate(energyStoredStr, currentAutonomy, worstCaseAutonomy);
                                "MorePowerInfo.Storage".Translate(energyStoredStr, currentAutonomyStr) + "\n" +
                                "MorePowerInfo.BatteryOnly".Translate(storageOnlyAutonomyStr);
 
+                    Utility.LogEveryNTicks("----");
 
                 }
 
@@ -108,3 +129,12 @@ namespace ExtraPowerInfo
 
 }
 
+static class Utility
+{
+    public static void LogEveryNTicks(string msg, int N = 60)
+    {
+#if DEBUG
+        if (GenTicks.TicksGame % N == 0) Log.Message(msg);
+#endif
+    }
+}
